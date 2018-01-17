3d3270cfd34ce2d6d0ff7c0939c4767ddf651126
unknown <student@IBLAB732P-10.ib.polsl.pl>
2018-01-17 14:29:58 +0100

    obsluga jednego buttona działa

--- app/src/main/java/com/example/student/rejestracjawizyt/Main2Activity.java
+++ app/src/main/java/com/example/student/rejestracjawizyt/Main2Activity.java
@@ -1,7 +1,15 @@
 package com.example.student.rejestracjawizyt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +17,17 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ButterKnife.bind(this);
+
+
+        ArrayList<Lekarz> lekarz = new ArrayList<>();
+        lekarz.add(new Lekarz("Jan Kowalski","dermatolog"));
+        lekarz.add(new Lekarz("Katarzyna Radecka","hepatolog"));
+        lekarz.add(new Lekarz("Agnieszka Domańska","chirurg plastyczny"));
+        lekarz.add(new Lekarz("Klaudia Nowak","kardiolog"));
+        lekarz.add(new Lekarz("Alicja Mazur","neurolog"));
+
+
     }
 }
--- app/src/main/java/com/example/student/rejestracjawizyt/MainActivity.java
+++ app/src/main/java/com/example/student/rejestracjawizyt/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.student.rejestracjawizyt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.bumptech.glide.Glide;
+import butterknife.BindView;
+import butterknife.OnClick;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_wybierzLekarza)
+    Button btn_wybierzlekarza ;
+
+    @OnClick(R.id.btn_wybierzLekarza)
+    void onClick1(View view){
+
+        Intent intent = new Intent( MainActivity.this, Main2Activity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
